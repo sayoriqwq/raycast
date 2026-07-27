63fc5f194b4a5721716fa1cc9e890b92e0e3ee02
sayoriqwq <2531600563@qq.com>
2026-07-28 01:04:36 +0800

    fix: 更新 Zed Nightly 与终端图标

---
+++ extensions/open-in-editor/src/open-in-zed-nightly.ts
@@ -0,0 +1,7 @@
+import { openInEditor, openInZedNewWindow } from "./lib";
+
+export default async function Command() {
+  await openInEditor("dev.zed.Zed-Nightly", "Zed Nightly", {
+    openPath: openInZedNewWindow,
+  });
+}
--- extensions/open-in-editor/src/open-in-zed-preview.ts
+++
@@ -1,7 +0,0 @@
-import { openInEditor, openInZedNewWindow } from "./lib";
-
-export default async function () {
-  await openInEditor("dev.zed.Zed-Preview", "Zed Preview", {
-    openPath: openInZedNewWindow,
-  });
-}
